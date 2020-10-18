84e5f20864907aa04b9f609f6c32cc51e01d2bca
Aleba <alexander.balykin1@gmail.com>
2020-10-18 20:58:18 +0300

    play game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const arr = [
+const arrStr = [
   'КРАСНЫЙ',
   'ЗЕЛЁНЫЙ',
   'СИНИЙ',
@@ -19,3 +19,33 @@ const arrColor = [
 ];
 
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomColor = () => {
+  const сolor = getRandomNum(0, arrColor.length - 1);
+  const colorFon = getRandomNum(0, arrColor.length - 1);
+  if (сolor === colorFon) {
+    return colorFon;
+  }
+  // document.getElementById('style').style.color = arrColor[сolor]; (Меняет цвет текста)
+  document.getElementById('style') // Возвращает ссылку на элемент по его идентификатору
+    .style.backgroundColor = arrColor[colorFon]; // Устанавливаем цвет фона элемента
+
+  return arrColor[colorFon];
+};
+
+const getRandomPosition = () => {
+  const maxWidth = window.innerWidth - 180;
+  const maxHeight = window.innerHeight - 50;
+  const elemId = document.getElementById('style');
+  elemId.style.left = `${getRandomNum(0, maxWidth)}px`;
+  elemId.style.top = `${getRandomNum(0, maxHeight)}px`;
+};
+
+const runGame = () => {
+  const randomStr = arrStr[getRandomNum(0, arrStr.length - 1)];
+  document.getElementById('style').innerHTML = randomStr; // Установка содержимого для элемента
+  getRandomColor();
+  getRandomPosition();
+};
+
+setInterval(runGame, 2000); // выполняет код раз в две секунды
